08ff02df209e8e8ded419ae8ee73443709f444fb
bloodyKnuckles <treenopie@gmail.com>
2016-03-04 12:17:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-var h = require('virtual-dom/h')
+//var h = require('virtual-dom/h')
 var main = require('main-loop')
 var shaved = require('shave-template')
+
+var template = fs.readFileSync('public/template.html', 'utf-8')
+
 var loop = main({ n: 0 }, render, require('virtual-dom'))
 document.querySelector('#content').appendChild(loop.target)
 
 function render (state) {
-  return h('div', [
-    h('h1', 'clicked ' + state.n + ' times'),
-    h('button', { onclick: onclick }, 'click me!')
-  ])
+  return shaved(template, {
+    '#count': state.n,
+    'button': {onclick: onclick}
+  })
   function onclick () {
     loop.update({ n: state.n + 1 })
   }
